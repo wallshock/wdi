5df3a082ff0bcd83cbfe54d484213eb819272fff
wallshock <wallshoccfella@gmail.com>
2021-10-13 22:27:10 +0200

    wersjaoptymalnadziala

--- fiboLP.py
+++ fiboLP.py
@@ -3,5 +3,25 @@ def f(n):
     for i in range(0, n):
         a, b = b, a + b
     return a
+#end
 
-print(f(6))
+target = int(input("Podaj sume docelową= "))
+sum = 0
+Lpointer = 1
+Rpointer = 1
+
+while(sum!=target):
+    if sum < target:
+        sum += f(Rpointer)
+        Rpointer += 1
+    elif sum > target and Lpointer != Rpointer:
+        sum -= f(Lpointer)
+        Lpointer += 1
+    else:
+        print("Nie ma")
+    #end
+#end
+
+for n in range(Lpointer,Rpointer):
+    print(f(n))
+#end
