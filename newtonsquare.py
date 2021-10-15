b4d84ce272647418120b2992876be20c1cdf955b
wallshock <wallshoccfella@gmail.com>
2021-10-15 22:16:21 +0200

    zabezpieczenia

--- newtonsquare.py
+++ newtonsquare.py
@@ -1,11 +1,15 @@
 def pierw(liczba,precyzja):
-    a, b = 1, liczba
-    pole = a*b
-    while (abs(a-b)>precyzja):
-        a = (a+b)/2
-        b = pole/a
-    #end
-    return a
+    if liczba < 0:
+        print("nie ma takiego pierw")
+        quit()
+    else:
+        a, b = 1, liczba
+        pole = a*b
+        while (abs(a-b)>precyzja):
+            a = (a+b)/2
+            b = pole/a
+        #end
+        return a
 #end
 
 num = 4
