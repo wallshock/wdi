e4b5e846185242a5d14b7dfa91dfa97b50c0164e
wallshock <wallshoccfella@gmail.com>
2021-10-25 13:14:57 +0200

    zwraca liste podzielnikow

--- podzielniki.py
+++ podzielniki.py
@@ -1,6 +1,8 @@
 def podzielniki(n):
+    lista = []
     for i in range(1,(n//2)+1):
-        if n%i == 0: print(i)
-    print(n)
+        if n%i == 0: lista.append(i)
+    lista.append(n)
+    return lista
     
 podzielniki(15)
